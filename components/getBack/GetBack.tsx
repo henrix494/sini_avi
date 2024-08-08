b0f8525f0ce0edcaea5394b753ba7071deea8780
natan <henrix494@gmail.com>
2024-08-08 19:17:32 +0300

    moved text to the right

--- components/getBack/GetBack.tsx
+++ components/getBack/GetBack.tsx
@@ -4,22 +4,22 @@ import Image from "next/image";
 export default function GetBack() {
   return (
     <div className="h-auto min-h-[800px] flex flex-col lg:flex-row justify-center items-center p-6 lg:p-10 max-lg:gap-10">
-      <div className="flex flex-col justify-start w-full lg:w-[600px] space-y-6 text-right lg:text-left max-lg:order-2 ">
+      <div className="flex flex-col justify-start w-full lg:w-[600px] space-y-6 text-right lg:text-left max-lg:order-2  ">
         <div>
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-main-bg mb-4">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-main-bg mb-4 text-right">
             !תחזרו למגרש שלכם
           </h1>
         </div>
 
         <div>
-          <p className="text-base md:text-lg lg:text-xl text-gray-700">
+          <p className="text-base md:text-lg lg:text-xl text-gray-700 text-right">
             האם ידעתם ששני שלישים מקבוצות ה-NFL משתמשות בדיקור סיני כדי לעזור
             להקל על השפעות אורח חייהן הפיזי המתיש?
           </p>
         </div>
 
         <div>
-          <p className="text-base md:text-lg lg:text-xl text-gray-700">
+          <p className="text-base md:text-lg lg:text-xl text-gray-700 text-right">
             across מדינה, יותר ספורטאים מקצועיים פונים לדיקור סיני כדי להקל על
             כיווץ שרירים וכאבים, וגם כדי להתמודד עם בעיות בריאותיות נוספות כמו
             אלרגיות, סטרס, דיכאון ובעיות שינה.
@@ -27,7 +27,7 @@ export default function GetBack() {
         </div>
 
         <div>
-          <p className="text-base md:text-lg lg:text-xl text-gray-700">
+          <p className="text-base md:text-lg lg:text-xl text-gray-700 text-right">
             אם דיקור סיני מועיל כל כך לספורטאים ברמה גבוהה, תארו לעצמכם מה הוא
             יכול לעשות בשבילכם. נסו אותו היום ואל תתנו לשום דבר לעצור אתכם
             מלהחזיר את עצמכם למגרש שלכם.
